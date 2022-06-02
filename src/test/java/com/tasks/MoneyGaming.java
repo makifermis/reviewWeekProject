39e57e41eeb3a9229c25febabb9c04b85c773f99
makifermis <makifermis@gmail.com>
2022-06-02 11:19:48 +0200

    MoneyGaming task completed

--- src/test/java/com/tasks/MoneyGaming.java
+++ src/test/java/com/tasks/MoneyGaming.java
@@ -3,7 +3,9 @@ package com.tasks;
 import com.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MoneyGaming {
@@ -12,13 +14,39 @@ public class MoneyGaming {
     @Test
     public void money_gaming_test(){
         driver = Driver.getDriver();
+        //Navigate to: https://moneygaming.qa.gameaccount.com/
         driver.get("https://moneygaming.qa.gameaccount.com/");
 
+        //Click the JOIN NOW button to open the registration page
         driver.findElement(By.xpath("//a[@class='newUser green']")).click();
 
-        //driver.findElement(By.xpath("//a[@class='newUser green']")).click();
+        //3. Select a title value from the dropdown
         Select select = new Select(driver.findElement(By.xpath("//select[@class='title required']")));
         select.selectByValue("Mr");
+
+        //Enter your first name and surname in the form
+        WebElement firstName = driver.findElement(By.xpath("//input[@id='forename']"));
+        firstName.sendKeys("Mehmet");
+
+        WebElement lastName = driver.findElement(By.xpath("//input[@name='map(lastName)']"));
+        lastName.sendKeys("Ermis");
+        //Check the tick box with text 'I accept the Terms and Conditions and certify that I am over
+        //the age of 18.'
+        driver.findElement(By.xpath("//input[@class='required checkbox terms']")).click();
+        //Submit the form by clicking the JOIN NOW button
+        driver.findElement(By.xpath("//input[@alt='Join Now']")).click();
+
+        // Validate that a validation message with text ‘ This field is required’ appears under the date of birth box
+
+        WebElement warningMessage = driver.findElement(By.xpath("//label[@for='dob']"));
+
+        String expectedMessage = "This field is required";
+        String actualMessage = warningMessage.getText();
+
+        Assert.assertEquals(actualMessage,expectedMessage);
+
+
+
 
     }
 
